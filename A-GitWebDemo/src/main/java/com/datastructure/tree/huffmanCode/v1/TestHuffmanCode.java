0dea4b557c9e394a20c3b42c13aff708ce470e4d
hbwillbebetter <784194196@qq.com>
2019-05-15 16:14:45 +0800

    霍夫曼文件压缩、文件解压

--- A-GitWebDemo/src/main/java/com/datastructure/tree/huffmanCode/v1/TestHuffmanCode.java
+++ A-GitWebDemo/src/main/java/com/datastructure/tree/huffmanCode/v1/TestHuffmanCode.java
@@ -1,10 +1,19 @@
 package com.datastructure.tree.huffmanCode.v1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipFile;
 
 import org.apache.logging.log4j.core.appender.rolling.action.ZipCompressAction;
 
@@ -16,22 +25,148 @@ public class TestHuffmanCode {
 	//用于存储霍夫曼编码
 	static Map<Byte, String> huffCodes = new HashMap<>();
 	public static void main(String[] args) {
+//		strZipAndUnzip();
+		String src = "img/1.bmp";
+		String dst = "img/2.zip";
+		try {
+//			zipFile(src,dst);
+			unZip(dst,"img/3.bmp");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void unZip(String src, String dst) throws Exception {
+		//创建一个输入流
+		InputStream is = new FileInputStream(src);
+		//因为压缩的时候，以对象流的形式压缩，那么读的时候，也以对象流的形式读取
+		ObjectInputStream ois = new ObjectInputStream(is);
+		//对象流的读取，和对象流之前的写入顺序要一致
+		//读取byte数组
+		byte[] b = (byte[]) ois.readObject();
+		//读取霍夫曼编码表
+		Map<Byte, String> codes = (Map<Byte, String>) ois.readObject();
+		ois.close();
+		is.close();
+		//使用霍夫曼进行解码
+		byte[] bytes = decode(codes, b);
+		//创建一个输出流
+		OutputStream os = new FileOutputStream(dst);
+		//写出数据
+		os.write(bytes);
+		os.close();
+	}
+
+	/**
+	 * 压缩文件
+	 * @param src
+	 * @param dst
+	 * @throws IOException
+	 */
+	private static void zipFile(String src, String dst) throws IOException {
+		//创建一个输入流
+		InputStream is = new FileInputStream(src);
+		//创建一个和输入流指向的文件大小一样的byte数组
+		byte[] b = new byte[is.available()];
+		//读取文件内容
+		is.read(b);
+		is.close();
+		//使用霍夫曼编码进行编码
+		byte[] byteZip = huffmanZip(b);
+		//输出流
+		OutputStream os = new FileOutputStream(dst);
+		ObjectOutputStream oos = new ObjectOutputStream(os);
+		//把压缩后的byte数组写入文件
+		oos.writeObject(byteZip);
+		//把霍夫曼编码表写入文件
+		oos.writeObject(huffCodes);
+		oos.close();
+		os.close();
+	}
+
+	/**
+	 * 字符串采用霍夫曼编码压缩、解压---能看到数据得到压缩
+	 */
+	public static void strZipAndUnzip(){
 		String msg="can you can a can as a can canner can a can.";
 		byte[] bytes = msg.getBytes();
 		//进行霍夫曼编码压缩
 		byte[] b = huffmanZip(bytes);
 //		System.out.println("压缩前字节数组长度:"+bytes.length);
 //		System.out.println("压缩后字节数组长度:"+b.length);
-		
-		
-		
-		
-		
-		
-		
-		System.out.println();
+		//使用霍夫曼编码进行解码
+		byte[] newBytes = decode(huffCodes,b);
+		System.out.println(new String(newBytes));
 	}
 	
+	/**
+	 * 使用指定的霍夫曼编码表进行解码
+	 * @param huffCodes2
+	 * @param b
+	 * @return
+	 */
+	private static byte[] decode(Map<Byte, String> huffCodes, byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		//把byte数组转为一个二进制字符串
+		for(int i=0; i<bytes.length; i++){//因为bytes最后一个很特殊，有可能不满足上面的8位，需要特殊处理
+			byte b = bytes[i];
+			//是否是最后一个
+			boolean flag = (i == bytes.length - 1);
+			sb.append(byteToBitStr(!flag,b));
+		}
+		//把字符串按照指定的霍夫曼编码进行解码
+		//把霍夫曼编码的键值进行调换
+		Map<String, Byte> map = new HashMap<>();
+		for(Map.Entry<Byte, String> entry : huffCodes.entrySet()){
+			map.put(entry.getValue(), entry.getKey());
+		}
+		//创建一个集合，用于存储解码后的byte
+		List<Byte> list = new ArrayList<>();
+		//处理字符串
+		for(int i=0; i<sb.length();){//11101001011010011011011001010111010010011011101001001110011100110111010010111010000110101110001011101001001101110100110111
+			int count = 1;
+			boolean flag = true;
+			Byte b = null;
+			//截取出一个byte
+			while (flag) {
+				String key = sb.substring(i, i+count);
+				b = map.get(key);
+				if (b == null) {
+					count++;
+				}else {
+					flag = false;
+				}
+			}
+			list.add(b);
+			i+=count;
+		}
+		//把集合转为数组
+		byte[] b = new byte[list.size()];
+		for(int i=0; i<list.size();i++){
+			b[i] = list.get(i);
+		}
+		return b;
+	}
+	
+	/**
+	 * byte转二进制字符串
+	 * @param flag
+	 * @param b
+	 * @return
+	 */
+	private static String byteToBitStr(boolean flag, byte b) {
+		int temp = b;
+		if(flag){//不是最后一个byte的处理
+			temp|=256;
+		}
+		String str = Integer.toBinaryString(temp);
+		if(flag){//非最后一个byte处理
+			return str.substring(str.length()-8);
+		}else{
+			return str;
+		}
+	}
+
 	/**
 	 * 进行霍夫曼编码压缩的方法
 	 * @param bytes
