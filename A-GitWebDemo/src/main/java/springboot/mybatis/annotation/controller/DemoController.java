3fbe38692709e48fdf0e2c0b44897bdb9228acc2
hbwillbebetter <784194196@qq.com>
2019-12-26 18:39:32 +0800

    mybatis更新优化部分...

--- A-GitWebDemo/src/main/java/springboot/mybatis/annotation/controller/DemoController.java
+++ A-GitWebDemo/src/main/java/springboot/mybatis/annotation/controller/DemoController.java
@@ -1,11 +1,14 @@
 package springboot.mybatis.annotation.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import springboot.mybatis.annotation.entity.Demo;
 import springboot.mybatis.annotation.mapper.DemoMapper;
+import springboot.mybatis.commonMapper.model.JsonData;
 
 @RestController
 public class DemoController {
@@ -21,6 +24,15 @@ public class DemoController {
 		demo.setAge(20);
 		mapper.save(demo);
 		return demo.toString();
+	}
+	
+	@RequestMapping("/getAll")
+	public JsonData getAll(){
+		List<Demo> all = mapper.getAll();
+		for(Demo demo: all){
+			System.out.println(demo.toString());
+		}
+		return JsonData.buildSuccess(all);
 	}
 	
 }
--- A-GitWebDemo/src/main/java/springboot/mybatis/annotation/mapper/DemoMapper.java
+++ A-GitWebDemo/src/main/java/springboot/mybatis/annotation/mapper/DemoMapper.java
@@ -1,14 +1,30 @@
 package springboot.mybatis.annotation.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 import springboot.mybatis.annotation.entity.Demo;
 
 @Mapper
 public interface DemoMapper {
+	//推荐使用#{}取值，不要用${},因为存在注入的风险
 	@Insert("insert into Demo(name,age) values(#{name},#{age})")  
+	//keyProperty java对象的属性，keyColumn表示数据库字段
     @Options(keyProperty="id",keyColumn="id",useGeneratedKeys=true)  
     public void save(Demo demo);
+	
+	@Select("select * from Demo")
+	@Results({
+			//数据字段的映射，我们在数据库内用下划线，开发的时候实体类不用下划线。所以就需要属性字段值和数据库的字段值进行映射
+//			@Result(column="create_time",property="createTime") //javaType=java.util.
+			@Result(column="name",property="name") //javaType=java.util.
+	})
+	List<Demo> getAll();
+	
 }
